8fd1ed275065601dd477978646d4d8142fded713
enkhturBaina <eegii.111.gg@gmail.com>
2023-07-31 16:46:40 +0800

    sidebar icon fix

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -3,49 +3,48 @@ import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/inde
 import { CImage } from '@coreui/react';
 import main_logo from 'src/assets/main_logo.png';
 import MainContext from 'src/context/MainContext';
+import CIcon from '@coreui/icons-react';
+import { cilAvTimer, cilBell, cilChatBubble, cilClipboard, cilGroup } from '@coreui/icons';
 
 const DefaultLayout = () => {
    const state = useContext(MainContext);
+   const [hoverItem, setHoverItem] = useState(null);
    const menus = [
       {
          id: 0,
          className: 'icon icon-b'
       },
       {
-         id: 1,
-         iconUrl: '/src/assets/images/sidebar/settings.svg'
+         id: 1
       },
       {
-         id: 2,
-         iconUrl: '/src/assets/images/sidebar/settings.svg'
+         id: 2
       },
       {
-         id: 3,
-         iconUrl: '/src/assets/images/sidebar/settings.svg'
+         id: 3
+      }
+   ];
+   const bottom_menus = [
+      {
+         id: 10,
+         className: 'icon icon-b',
+         icon: cilAvTimer
       },
       {
-         id: 4,
-         iconUrl: '/src/assets/images/sidebar/settings.svg'
+         id: 11,
+         icon: cilChatBubble
       },
       {
-         id: 5,
-         iconUrl: '/src/assets/images/sidebar/settings.svg',
-         isBottom: true
+         id: 12,
+         icon: cilGroup
       },
       {
-         id: 6,
-         iconUrl: '/src/assets/images/sidebar/settings.svg',
-         isBottom: true
+         id: 13,
+         icon: cilBell
       },
       {
-         id: 7,
-         iconUrl: '/src/assets/images/sidebar/settings.svg',
-         isBottom: true
-      },
-      {
-         id: 8,
-         iconUrl: '/src/assets/images/sidebar/settings.svg',
-         isBottom: true
+         id: 14,
+         icon: cilAvTimer
       }
    ];
    return (
@@ -61,13 +60,54 @@ const DefaultLayout = () => {
                            className={state.selectedParentMenu === index ? 'active' : ''}
                            onClick={() => state.setSelectedParentMenu(item.id)}
                         >
-                           <div className="content">
+                           <div
+                              className={hoverItem === item.id ? 'content hover-style' : 'content'}
+                              onMouseEnter={() => {
+                                 setHoverItem(item.id);
+                              }}
+                              onMouseLeave={() => {
+                                 setHoverItem(null);
+                              }}
+                           >
                               <span
                                  className={
                                     state.selectedParentMenu === index
                                        ? 'icon icon-b disable-caret active'
                                        : 'icon icon-b disable-caret'
                                  }
+                              />
+                           </div>
+                        </li>
+                     );
+                  })}
+               </ul>
+            </div>
+            <div className="bottom">
+               <ul>
+                  {bottom_menus.map((item, index) => {
+                     return (
+                        <li
+                           key={index}
+                           className={state.selectedParentMenu === item.id ? 'active' : ''}
+                           onClick={() => state.setSelectedParentMenu(item.id)}
+                        >
+                           <div
+                              className={hoverItem === item.id ? 'content hover-style' : 'content'}
+                              onMouseEnter={() => {
+                                 setHoverItem(item.id);
+                              }}
+                              onMouseLeave={() => {
+                                 setHoverItem(null);
+                              }}
+                           >
+                              <CIcon
+                                 icon={item.icon}
+                                 className={
+                                    hoverItem === item.id || state.selectedParentMenu === item.id
+                                       ? 'main-color'
+                                       : 'text-white'
+                                 }
+                                 size="lg"
                               />
                            </div>
                         </li>
