fae7e39490fd9ffbd170f0f26c6395c6c5f40491
enkhturBaina <eegii.111.gg@gmail.com>
2023-08-09 10:03:39 +0800

    zaswar

--- src/views/Attendance/attendance/Tab1.js
+++ src/views/Attendance/attendance/Tab1.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import Toolbar from './Toolbar';
-import { Table } from 'antd';
+import { Badge, Table, Tag } from 'antd';
 import avatar from '../../../assets/images/avatars/1.jpg';
+import { MoreOutlined } from '@ant-design/icons';
 
 function Tab1() {
    const dataSource = [
       {
          key: '1',
          name: 'Mike',
-         age: 32,
-         address: '10 Downing Street',
-         regnum: 'АБ11223344'
+         company_name: 'Downing Street',
+         company_reg: 32,
+         emp_department: '10 Downing Street',
+         regnum: 'АБ11223344',
+         att_day: 'att_day',
+         schedule: 'Чөлөөт',
+         status: 'Томилолт',
+         in_time: '09:00/09:05',
+         out_time: '18:00/18:00',
+         work_time: '9',
+         active_time: '0',
+         more_time: '0',
+         x_time: '5 мин'
       },
       {
          key: '2',
@@ -26,6 +37,7 @@ function Tab1() {
          align: 'center',
          dataIndex: 'name',
          key: 'name',
+         align: 'center',
          render: (_, record) => (
             <div className="flex flex-row items-center">
                <img src={avatar} width={30} height={30} style={{ borderRadius: '50%' }} />
@@ -40,58 +52,86 @@ function Tab1() {
          title: <span className="text-gray-400">Компанийн нэр,рд</span>,
          align: 'center',
          dataIndex: 'company_reg',
-         key: 'company_reg'
+         key: 'company_reg',
+         align: 'center',
+         render: (_, record) => (
+            <div className="flex flex-col !ml-2">
+               <span>{record.company_name}</span>
+               <span className="text-gray-400 text-xs">{record.company_reg}</span>
+            </div>
+         )
       },
       {
          title: <span className="text-gray-400">Хэлтэс, албан тушаал</span>,
          align: 'center',
          dataIndex: 'emp_department',
-         key: 'emp_department'
+         key: 'emp_department',
+         align: 'center'
       },
       {
          title: <span className="text-gray-400">Өдөр</span>,
-         dataIndex: 'name',
-         key: 'name'
+         dataIndex: 'att_day',
+         key: 'att_day',
+         align: 'center'
       },
       {
          title: <span className="text-gray-400">Хуваарь</span>,
-         dataIndex: 'age',
-         key: 'age'
+         dataIndex: 'schedule',
+         key: 'schedule',
+         align: 'center',
+         render: (_, record) => (
+            <Badge status="success" text={<span className="text-green-500">{record.schedule}</span>} />
+         )
       },
       {
          title: <span className="text-gray-400">Төлөв</span>,
-         dataIndex: 'address',
-         key: 'address'
+         dataIndex: 'status',
+         key: 'status',
+         align: 'center',
+         render: (_, record) => <Tag color="volcano">{record.status}</Tag>
       },
       {
          title: <span className="text-gray-400">Ирэх/Ирсэн</span>,
-         dataIndex: 'address',
-         key: 'address'
+         dataIndex: 'in_time',
+         key: 'in_time',
+         align: 'center'
       },
       {
          title: <span className="text-gray-400">Явах/Явсан</span>,
-         dataIndex: 'address',
-         key: 'address'
+         dataIndex: 'out_time',
+         key: 'out_time',
+         align: 'center'
       },
       {
          title: <span className="text-gray-400">Ажилласан цаг</span>,
-         dataIndex: 'address',
-         key: 'address'
+         dataIndex: 'work_time',
+         key: 'work_time',
+         align: 'center'
       },
       {
          title: <span className="text-gray-400">Идэвхийн цаг</span>,
-         dataIndex: 'address',
-         key: 'address'
+         dataIndex: 'active_time',
+         key: 'active_time',
+         align: 'center'
       },
       {
          title: <span className="text-gray-400">Илүү цаг</span>,
-         dataIndex: 'address',
-         key: 'address'
+         dataIndex: 'more_time',
+         key: 'more_time',
+         align: 'center'
       },
       {
          title: <span className="text-gray-400">Х/цаг</span>,
-         dataIndex: 'address',
-         key: 'address'
+         dataIndex: 'x_time',
+         key: 'x_time',
+         align: 'center'
+      },
+      {
+         title: <MoreOutlined />,
+         dataIndex: 'x_time',
+         key: 'x_time',
+         align: 'center',
+         render: (_, record) => <MoreOutlined className="cursor-pointer" />
       }
    ];
    return (
