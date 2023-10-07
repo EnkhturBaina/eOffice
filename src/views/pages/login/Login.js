758a8e76d05919b713cdd260624f400acef73586
enkhturBaina <eegii.111.gg@gmail.com>
2023-10-07 13:44:55 +0800

    login fix plss

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import AuthContext from 'src/context/AuthContext';
 import login_logo from '../../../assets/login_logo.png';
-import { Input, Checkbox, Button } from 'antd';
+import { Input, Checkbox, Button, Form } from 'antd';
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import CIcon from '@coreui/icons-react';
 import { cilArrowRight } from '@coreui/icons';
@@ -14,8 +14,10 @@ import linkedin from '../../../assets/images/login/linkedin.png';
 
 const Login = () => {
    const navigate = useNavigate();
+   const [loginForm] = Form.useForm();
    const { login, user } = useContext(AuthContext);
    const onFinish = async (values) => {
+      console.log('res', res);
       const res = await login(values);
    };
    // newtertsn ued login ruu oroh ued butsaagad dashboardru ywulah
@@ -36,20 +38,48 @@ const Login = () => {
                Байгууллагын нөөцийн удирдлагын системд тавтай морил
             </span>
             <span className="text-xl font-semibold text-center !p-2 !mx-5 !my-4 !mt-1">Нэвтрэх</span>
+
             <div className="flex mt-1 flex-col w-1/5">
-               <div className="basis-1/5">
-                  <span className="text-xs mt-1">Имэйл</span>
-                  <Input placeholder="Имэйл" className="mt-1" size="large" />
-               </div>
-               <div className="basis-1/5 !mt-2">
-                  <span className="text-xs mt-1">Нууц үг</span>
-                  <Input.Password
-                     placeholder="*************"
-                     className="mt-1"
-                     size="large"
-                     iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-                  />
-               </div>
+               <Form form={loginForm}>
+                  <div className="basis-1/5">
+                     <span className="text-xs mt-1">Имэйл</span>
+                     <Form.Item
+                        // noStyle
+                        rules={[
+                           {
+                              required: true,
+                              message: 'Имэйл заавал'
+                           },
+                           {
+                              type: 'email',
+                              message: 'Имэйл хэлбэр буруу'
+                           }
+                        ]}
+                        name="email"
+                     >
+                        <Input type="email" placeholder="Имэйл" className="mt-1" size="large" />
+                     </Form.Item>
+                  </div>
+                  <div className="basis-1/5 !mt-2">
+                     <span className="text-xs mt-1">Нууц үг</span>
+                     <Form.Item
+                        rules={[
+                           {
+                              required: true,
+                              message: 'Нууц үг заавал'
+                           }
+                        ]}
+                        name="password"
+                     >
+                        <Input.Password
+                           placeholder="*************"
+                           className="mt-1"
+                           size="large"
+                           iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                        />
+                     </Form.Item>
+                  </div>
+               </Form>
             </div>
             <div className="flex flex-row justify-between w-1/5 my-2">
                <Checkbox onChange={onChange}>Намайг сана</Checkbox>
@@ -62,9 +92,8 @@ const Login = () => {
                className="w-1/5 flex items-center justify-center"
                size="large"
                onClick={() =>
-                  onFinish({
-                     email: 'test',
-                     password: 'test'
+                  loginForm.validateFields().then(async (values) => {
+                     await login(values);
                   })
                }
             >
